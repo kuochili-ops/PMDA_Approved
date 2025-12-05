3469b7e9fb6b56226f004525a1a85e10684a4a8f
kuochili-ops <kuochili@gmail.com>
2025-12-05 09:38:53 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -16,3 +16,18 @@ def get_pubchem_synonyms(japanese_name):
 # 範例查詢
 synonyms = get_pubchem_synonyms("アセトアミノフェン")
 print(synonyms)
+
+def get_pubchem_iupac(japanese_name):
+    # PubChem API: 根據成分名稱查詢 IUPAC 學名
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{japanese_name}/property/IUPACName/JSON"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        iupac_name = data.get('PropertyTable', {}).get('Properties', [{}])[0].get('IUPACName', '')
+        return iupac_name
+    else:
+        print("查詢失敗，請確認成分名稱或網路連線。")
+        return ""
+
+# 範例查詢
+iupac_name = get_pubchem_iupac("アセトアミノフェン")
